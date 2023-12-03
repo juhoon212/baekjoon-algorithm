7b41ae478a1d42044e8ccc2bf33795e3b8cc974c
J.H <121741140+juhoon212@users.noreply.github.com>
2023-12-03 21:25:47 +0900

    [Bronze II] Title: 수 정렬하기, Time: 192 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,33 +1,33 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 class Main {
 
 
     public static void main(String[] args) throws IOException {
 
-        // 오름차순 정렬하라
+        // 들어오는 값 오름차순 정렬
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // N개의 수
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
-        // 배열에 들어오는 수 저장
+        // 버블정렬 사용
+
+        int[] arr = new int[N];
+        
+        
+        // 배열에 저장
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // N - 1 => 맨끝에 가서는 연산을 하지 않으니까.
         for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i ; j++) {
-
+            for (int j = 0; j < N - 1 - i; j++) {
                 if(arr[j] > arr[j + 1]) {
-                    // 스왑연산
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -38,6 +38,8 @@ class Main {
         for (int i = 0; i < N; i++) {
             System.out.println(arr[i]);
         }
+
+        
 
     }
 }

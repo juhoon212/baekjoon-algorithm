3c1c8d04e975e97f2109f724bc72a3c3d7d1e62f
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-10 12:15:02 +0900

    [Gold V] Title: 집합의 표현, Time: 1292 ms, Memory: 60272 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,33 +1,41 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.*;
+public class Main {
 
+    static int[] presentNode;
 
-class Main {
+    public static void main(String[] args) throws IOException {
 
-    static int parent[]; // 대표 노드
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        parent = new int[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            parent[i] = i;
+        presentNode = new int[n + 1];
+
+        // 대표노드 초기화
+        for (int i = 1; i <= n ; i++) {
+            presentNode[i] = i;
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            int question = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            int unionOrFind = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if(question == 0) {
+            if (unionOrFind == 0) {
                 union(a, b);
             } else {
                 boolean result = checkSame(a, b);
-                if(result) {
+
+                if (result) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
@@ -40,33 +48,28 @@ class Main {
         a = find(a);
         b = find(b);
 
-        if(a == b) return true;
-
+        if(a == b) {
+            return true;
+        }
 
         return false;
     }
 
     private static void union(int a, int b) {
-
+        
         a = find(a);
         b = find(b);
 
-        if(a != b) {
-            parent[a] = b;
+        if (presentNode[a] != presentNode[b]) {
+            presentNode[a] = b;
         }
     }
 
-
-    private static int find(int a) {
-
-        if(a == parent[a]) {
-            return a;
+    private static int find(int num) {
+        if (presentNode[num] == num) {
+            return num;
         } else {
-            return parent[a] = find(parent[a]);
+            return presentNode[num] = find(presentNode[num]);
         }
     }
-
-
-
-
 }

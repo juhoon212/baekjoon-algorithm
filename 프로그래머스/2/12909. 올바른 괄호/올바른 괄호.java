948599f489d60819477e1295784e337c3e0130b1
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-14 09:58:14 +0900

    [level 2] Title: 올바른 괄호, Time: 5.99 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+     boolean solution(String s) {
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')'){
+                count--;
+            }
+
+            if (count < 0) {
+                break;
+            }
+        }
+
+
+
+        if (count == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+        
+}

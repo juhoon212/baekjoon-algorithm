5d42b960b50e6e65e795dd908d584283fc4e02b1
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-20 10:26:14 +0900

    [level 2] Title: 피보나치 수, Time: 3.16 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,20 @@
+
+import java.math.BigInteger;
+
+public class Solution {
+
+    public int solution(int n) {
+        int answer = 1;
+        int f1 = 0, f2 = 1;
+
+        for(int i = 2; i<=n; i++){
+            answer = (f1 + f2)%1234567;
+            f1 = f2;
+            f2 = answer;
+        }
+
+        //if(n>=2) answer %= 1234567;
+        return answer;
+    }
+
+}

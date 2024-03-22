30c5abc75a0928747cb6964a1b1eef7893e876e2
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-22 17:47:24 +0900

    [Gold V] Title: 최소비용 구하기, Time: 564 ms, Memory: 50716 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,94 +1,94 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
-class Node implements Comparable<Node> {
+
+class Nodes2 {
     int end;
     int weight;
- 
-    Node(int end, int weight) {
+
+    public Nodes2(int end, int weight) {
         this.end = end;
         this.weight = weight;
     }
- 
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
- 
 }
- 
+
 public class Main {
-    static int N, M;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
- 
-    public static void main(String[] args) throws Exception {
+
+    static ArrayList<Nodes2>[] graph;
+    static boolean[] visited;
+    static long[] distance;
+
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
- 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        Arrays.fill(dist, Integer.MAX_VALUE);
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        distance = new long[N + 1];
+
+        // distance 배열 초기화
+        for (int i = 1; i <= N; i++) {
+            distance[i] = Long.MAX_VALUE;
+            graph[i] = new ArrayList<>();
         }
- 
-        // 단방향 인접 리스트 구현.
+
+        StringTokenizer st;
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[start].add(new Nodes2(end, cost));
         }
- 
+
         st = new StringTokenizer(br.readLine());
-        int startPos = Integer.parseInt(st.nextToken());
-        int endPos = Integer.parseInt(st.nextToken());
- 
-        bw.write(dijkstra(startPos, endPos) + "\n");
-        bw.flush();
-        bw.close();
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start, end);
+
         br.close();
     }
- 
-    // 다익스트라 알고리즘
-    public static int dijkstra(int start, int end) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
- 
-            if (!check[cur]) {
-                check[cur] = true;
- 
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
-                    }
+
+    private static void dijkstra(int start, int end) {
+
+        distance[start] = 0;
+
+        PriorityQueue<Nodes2> queue = new PriorityQueue<>(
+                (o1, o2) -> o1.weight - o2.weight
+        );
+
+        queue.offer(new Nodes2(start, 0));
+
+        while (!queue.isEmpty()) {
+            Nodes2 now = queue.poll();
+            if (!visited[now.end]) {
+                visited[now.end] = true;
+            
+
+            for (int i = 0; i < graph[now.end].size(); i++) {
+                Nodes2 next = graph[now.end].get(i);
+
+                if (!visited[next.end]
+                        && distance[next.end] > now.weight + next.weight
+                ) {
+                    distance[next.end] = now.weight + next.weight;
+                    queue.offer(new Nodes2(next.end, (int) distance[next.end]));
                 }
             }
         }
-        return dist[end];
+        }
+            System.out.println(distance[end]);
     }
 }

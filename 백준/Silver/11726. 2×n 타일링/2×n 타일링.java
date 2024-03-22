77c2fd4fe7c09c66e0a358a31758bd3801322b58
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-22 14:24:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,9 +1,14 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         // n일때
         // n-1
         // n-2 ... n

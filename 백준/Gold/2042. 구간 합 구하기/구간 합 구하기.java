e658f9dff9059da81447b80a7905a5f0c37aa4d6
J.H <121741140+juhoon212@users.noreply.github.com>
2024-03-29 11:54:20 +0900

    [Gold I] Title: 구간 합 구하기, Time: 712 ms, Memory: 95424 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -5,99 +5,94 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- *  <세그먼트 트리> => 수의 변경이나 합이 빈번히 일어남
- *  1. 트리 초기화
- *  2. 부모 노드값 채우기
- *  3. 질의 값 구하기 => (start + 1 / 2) , (end - 1 / 2)
- *  4. 업데이트
- */
-
-
 public class Main {
 
-    // -2^63 >= 주어진수 <= 2^63-1
     static long[] tree;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long N = Long.parseLong(st.nextToken()); // 수의 개수
-        long M = Long.parseLong(st.nextToken()); // 수의 변경 횟수
-        long K = Long.parseLong(st.nextToken()); // 구간 합 횟수
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
 
+        // 1. 트리 초기화
         long treeHeight = 0;
-        long temp = N;
 
+        long temp = N;
         while (temp > 0) {
             temp /= 2;
             treeHeight++;
         }
 
         long treeSize = (long) Math.pow(2, treeHeight + 1);
+
+        tree = new long[(int)treeSize];
+
         long startNodeIndex = treeSize / 2;
 
-        tree = new long[(int) treeSize];
-
-        for (int i = (int) startNodeIndex; i < startNodeIndex + N; i++) {
-            tree[i] = Long.parseLong(br.readLine());
+        for (int i = (int)startNodeIndex; i < startNodeIndex + N; i++) {
+            tree[i] =  Long.parseLong(br.readLine());
         }
-
-        setTree(treeSize - 1);
+        // 2. 부모 노드 초기화 및 설정
+        long index = treeSize - 1;
+        while (index > 0) {
+            tree[(int)index / 2] += tree[(int)index];
+            index--;
+        }
+        // 3. 질의 값 구하기
 
         for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
 
-            long a = Long.parseLong(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
+             long a = Long.parseLong(st.nextToken());
+             long b = Long.parseLong(st.nextToken());
+             long c = Long.parseLong(st.nextToken());
 
             if (a == 1) {
-                changeVal(b + startNodeIndex - 1, c);
-            } else if (a == 2) {
-                long result = getSum(b + startNodeIndex - 1, c + startNodeIndex - 1);
+                // 트리의 인덱스로 변환해야함
+                // 해당 index + 2 * treeHeight - 1
+                update(b + startNodeIndex - 1, c);
+            } else if (a== 2){
+                long result = sumValue(b + startNodeIndex - 1,  c + startNodeIndex - 1);
                 System.out.println(result);
             }
         }
 
         br.close();
-
     }
 
-    private static void setTree(long num) {
+    private static long sumValue(long start, long end) {
 
-        while (num > 0) {
-            tree[(int) num / 2] += tree[(int)num];
-            num--;
-        }
-    }
-
-    private static void changeVal(long index, long value) {
-        long diff = value - tree[(int) index];
-
-        while (index > 0) {
-            tree[(int) index] += diff;
-            index /= 2;
-        }
-    }
-
-    private static long getSum(long start, long end) {
-        long result = 0;
+        long sum = 0;
+        // 선택
         while (start <= end) {
+
             if (start % 2 == 1) {
-                result += tree[(int) start];
+                sum += tree[(int)start];
             }
 
             if (end % 2 == 0) {
-                result += tree[(int) end];
+                sum += tree[(int) end];
             }
 
             start = (start + 1) / 2;
             end = (end - 1) / 2;
         }
 
-        return result;
+        return sum;
     }
+
+    private static void update(long index, long value) {
+        long diff = value - tree[(int)index];
+
+        while (index > 0) {
+            tree[(int)index] += diff;
+            index /= 2;
+        }
+
+    }
+
+
 }

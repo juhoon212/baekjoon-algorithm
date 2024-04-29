ff78766ea89d9662752651f8e86cc8e4096cff01
J.H <121741140+juhoon212@users.noreply.github.com>
2024-04-29 12:11:42 +0900

    [Platinum III] Title: 달리기, Time: 544 ms, Memory: 75084 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.java
+++ 백준/Platinum/16930. 달리기/달리기.java
@@ -8,15 +8,13 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static boolean[][] visited;
+    static char[][] graph;
+    static int[][] distance;
+    static int N, M, K;
+
     static int[] dx = {1, 0, 0, -1};
     static int[] dy = {0, 1, -1, 0};
-
-    static boolean[][] visited;
-    static String[][] graph;
-    static int[][] graph2;
-
-
-    static int N, M, K;
 
     public static void main(String[] args) throws IOException {
 
@@ -27,65 +25,61 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        graph = new String[N + 1][M + 1];
-        graph2 = new int[N + 1][M + 1];
         visited = new boolean[N + 1][M + 1];
+        graph = new char[N + 1][M + 1];
+        distance = new int[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             String line = st.nextToken();
+
             for (int j = 1; j <= M; j++) {
-                graph[i][j] = line.substring(j - 1, j);
+                graph[i][j] = line.charAt(j - 1);
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        final int sx = Integer.parseInt(st.nextToken());
-        final int sy = Integer.parseInt(st.nextToken());
-        final int ex = Integer.parseInt(st.nextToken());
-        final int ey = Integer.parseInt(st.nextToken());
+        final int startRow = Integer.parseInt(st.nextToken());
+        final int startCol = Integer.parseInt(st.nextToken());
+        final int endRow = Integer.parseInt(st.nextToken());
+        final int endCol = Integer.parseInt(st.nextToken());
 
-        bfs(sx, sy, ex, ey);
-        if (graph2[ex][ey] == 0) {
+        bfs(startRow, startCol, endRow, endCol);
+        if (distance[endRow][endCol] == 0) {
             System.out.println("-1");
-        } else {
-            System.out.println(graph2[ex][ey]);
+        }else {
+            System.out.println(distance[endRow][endCol]);
         }
-
-        br.close();
     }
 
-    private static void bfs(int sx, int sy, int ex, int ey) {
+    private static void bfs(int startRow, int startCol, int endRow, int endCol) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{sx, sy});
+        queue.add(new int[]{startRow, startCol});
+        visited[startRow][startCol] = true;
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
             int curRow = poll[0];
             int curCol = poll[1];
-            visited[curRow][curCol] = true;
+//            visited[curRow][curCol] = true;
 
             for (int i = 0; i < 4; i++) {
                 for (int j = 1; j <= K; j++) {
-                    final int newRow = curRow + dx[i] * j;
-                    final int newCol = curCol + dy[i] * j;
+                    int newRow = curRow + dx[i] * j;
+                    int newCol = curCol + dy[i] * j;
 
-                    if (newRow >= 1 && newRow <= N && newCol <= M && newCol >= 1 && graph[newRow][newCol].equals(".")) {
+                    if (newRow >= 0 && newRow <= N && newCol > 0 && newCol <= M && graph[newRow][newCol] == '.') {
                         if (!visited[newRow][newCol]) {
                             visited[newRow][newCol] = true;
-                            graph2[newRow][newCol] = graph2[curRow][curCol] + 1;
+                            distance[newRow][newCol] = distance[curRow][curCol] + 1;
 
-                            if (newRow == ex && newCol == ey) {
-                                return;
-                            }
+                            if (newRow == endRow && newCol == endCol) return;
+
                             queue.add(new int[]{newRow, newCol});
-                        } else if (graph2[newRow][newCol] <= graph2[curRow][curCol]) break;
+                        }else if (distance[newRow][newCol] <= distance[curRow][curCol]) break;
                     } else break;
                 }
             }
         }
     }
-
-
-
 }

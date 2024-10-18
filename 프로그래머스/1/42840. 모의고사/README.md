b6bc28ad1893d708b159c8b6385cbccfa6a661fc
Joo Hoon Yun <121741140+juhoon212@users.noreply.github.com>
2024-10-18 22:47:36 +0900

    [level 1] Title: 모의고사, Time: 61.38 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 3.69 ms
+메모리: 93.8 MB, 시간: 61.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 17일 23:55:05
+2024년 10월 18일 22:47:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,21 +1,59 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answer) {
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] score = new int[3];
-        for(int i=0; i<answer.length; i++) {
-            if(answer[i] == a[i%a.length]) {score[0]++;}
-            if(answer[i] == b[i%b.length]) {score[1]++;}
-            if(answer[i] == c[i%c.length]) {score[2]++;}
+   public int[] solution(int[] answer) {
+
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        List<Integer> score = new ArrayList<>();
+        score.add(0, 0);
+        score.add(1, 0);
+        score.add(2, 0);
+
+        System.out.println("size = " + score.size());
+        // 규칙
+        // 답이 {1,2,3,4,5} 라고 가정
+        for (int i = 0; i < answer.length; ++i) {
+            if (one[i % one.length] == answer[i]) {
+                score.set(0, score.get(0) + 1);
+                System.out.println("one = " + score.get(0));
+                System.out.println("size = " + score.size());
+            }
+
+            if (two[i % two.length] == answer[i]) {
+                score.set(1, score.get(1) + 1);
+                System.out.println("two = " + score.get(1));
+                System.out.println("size = " + score.size());
+            }
+
+            if (three[i % three.length] == answer[i]) {
+                score.set(2, score.get(2) + 1);
+                System.out.println("three = " + score.get(2));
+                System.out.println("size = " + score.size());
+            }
         }
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-        ArrayList<Integer> list = new ArrayList<>();
-        if(maxScore == score[0]) {list.add(1);}
-        if(maxScore == score[1]) {list.add(2);}
-        if(maxScore == score[2]) {list.add(3);}
-        return list.stream().mapToInt(i->i.intValue()).toArray();
+
+        for (int i = 0; i < score.size(); ++i) {
+            System.out.println(score.get(i));
+        }
+        System.out.println("===============");
+
+        int max;
+        max = Math.max(Math.max(score.get(0), score.get(1)), score.get(2));
+
+        // 가장 많은 문제를 맞추는 사람을 배열에 담아서 return
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < 3; ++i) {
+            if (max == score.get(i)) {
+                result.add(i + 1);
+            }
+        }
+
+        return result.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }

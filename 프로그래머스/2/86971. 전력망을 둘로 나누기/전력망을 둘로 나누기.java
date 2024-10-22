5ea23ff05d3cb494d8fa00c09a07557b5ba81d10
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2024-10-23 00:08:06 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.29 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,63 +1,75 @@
 import java.util.*;
 
 class Solution {
-    static int n;
-    static int [][] wires;
+     int n;
+    int[][] wires;
+    
+    // 계속 초기화
+    boolean[] visited;
+    List<Integer>[] tree;
 
-    static ArrayList<Integer>[] A;
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
-
+    int min = Integer.MAX_VALUE;
+    
+    
     public int solution(int n, int[][] wires) {
-        this.n = n;
-        this.wires = wires;
-
-        A = new ArrayList[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            A[i] = new ArrayList<>();
+        // 트리 형태로 되어있다.
+        init(n, wires);
+        // 트리 초기화
+        for (int i = 1; i <= n; ++i) {
+            tree[i] = new ArrayList<>();
         }
-
-        // 양방향
-        for (int i = 0; i < wires.length; i++) {
-            int a = wires[i][0];
-            int b = wires[i][1];
-            A[a].add(b);
-            A[b].add(a);
-        }
-
-        // 모든 간선에 대해 한 번씩 끊어봄(완전탐색)
-        for (int i = 0; i < wires.length; i++) {
+        // 양방향? 단방향? 
+        for (int i = 0; i < wires.length; ++i) {
             int a = wires[i][0];
             int b = wires[i][1];
 
-            // 반복문 돌때마다 매번 새롭게 생성
+            tree[a].add(b);
+            tree[b].add(a);
+        }
+
+        // 전선 하나를 끊는다. -> edge 하나를 끊는다.
+        for (int i = 0; i < wires.length; ++i) {
             visited = new boolean[n + 1];
+            
+            int a = wires[i][0];
+            int b = wires[i][1];
+            
+            // edge 삭제
+            tree[a].remove(Integer.valueOf(b));
+            tree[b].remove(Integer.valueOf(a));
+            // 1부터 순회
+            int result = dfs(1);
 
-            // 1. 해당 간선을 그래프에서 제거
-            A[a].remove(Integer.valueOf(b));
-            A[b].remove(Integer.valueOf(a));
-
-            int cnt = dfs(1);
-            int diff = Math.abs(cnt - (n - cnt)); // |송전탑 개수 차이 계산| -> 최솟값 갱신
+            // 송전탑 즉 노드의 갯수가 가능한 비슷하도록 나눈다.
+            // 송전탑 그룹 1 - 송전탑 그룹 2 -> 의 절대값이 제일작은것
+            int diff = Math.abs(result - (n - result));
             min = Math.min(min, diff);
-
-            A[a].add(b);
-            A[b].add(a);
+            
+            // 원상 복구
+            tree[a].add(b);
+            tree[b].add(a);
         }
-
+        
         return min;
     }
-
+    
     private int dfs(int value) {
+        
         visited[value] = true;
         int count = 1;
-
-        for (int next: A[value]) {
+        for (int next : tree[value]) {
             if (!visited[next]) {
                 count += dfs(next);
             }
         }
+        
         return count;
+    }
+    
+    private void init(int n, int[][] wires) {
+        this.n = n;
+        this.wires = wires;
+        // visited 계속 초기화
+        tree = new ArrayList[n + 1];
     }
 }

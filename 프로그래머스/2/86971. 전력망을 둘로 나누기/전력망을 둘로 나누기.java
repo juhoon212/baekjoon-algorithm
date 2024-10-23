4228a646d53877947da3fd08677612cb593d6e90
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2024-10-23 21:55:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.38 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
 class Solution {
-     int n;
+    int n;
     int[][] wires;
-    
-    // 계속 초기화
-    boolean[] visited;
-    List<Integer>[] tree;
+    ArrayList<Integer>[] tree;
+
+    boolean visited[];
 
     int min = Integer.MAX_VALUE;
-    
-    
+
     public int solution(int n, int[][] wires) {
-        // 트리 형태로 되어있다.
         init(n, wires);
-        // 트리 초기화
+        // 트리 형태로 되어있다.
+        // 트리 초기화 -> 1 ~ 9 => wires 안에 들어있는 수가 1~9
         for (int i = 1; i <= n; ++i) {
             tree[i] = new ArrayList<>();
         }
-        // 양방향? 단방향? 
+
+        // 인접리스트 (양방향 그래프)
         for (int i = 0; i < wires.length; ++i) {
             int a = wires[i][0];
             int b = wires[i][1];
@@ -27,34 +26,35 @@ class Solution {
             tree[b].add(a);
         }
 
-        // 전선 하나를 끊는다. -> edge 하나를 끊는다.
         for (int i = 0; i < wires.length; ++i) {
-            visited = new boolean[n + 1];
-            
+            // 1 부터 계속 edge 를 끊으면서 완전 탐색.
             int a = wires[i][0];
             int b = wires[i][1];
-            
-            // edge 삭제
+
+            // 방문 배열도 0 ~ 9 까지니까 10으로 세팅
+            visited = new boolean[n + 1];
+
+            // edge 끊어준다.
+            // remove(Object) 라서 Wrapper 클래스로 변환
             tree[a].remove(Integer.valueOf(b));
             tree[b].remove(Integer.valueOf(a));
-            // 1부터 순회
-            int result = dfs(1);
 
-            // 송전탑 즉 노드의 갯수가 가능한 비슷하도록 나눈다.
-            // 송전탑 그룹 1 - 송전탑 그룹 2 -> 의 절대값이 제일작은것
+            // 1부터 시작
+            int result = dfs(1);
+            // 잘라진 두 노드가 제일 비슷한 값을 찾는다.(절대값)
             int diff = Math.abs(result - (n - result));
             min = Math.min(min, diff);
             
-            // 원상 복구
+            // 복구
             tree[a].add(b);
             tree[b].add(a);
         }
-        
+
         return min;
     }
-    
+
+    // 탐색 dfs
     private int dfs(int value) {
-        
         visited[value] = true;
         int count = 1;
         for (int next : tree[value]) {
@@ -62,14 +62,13 @@ class Solution {
                 count += dfs(next);
             }
         }
-        
+
         return count;
     }
-    
+
     private void init(int n, int[][] wires) {
         this.n = n;
         this.wires = wires;
-        // visited 계속 초기화
-        tree = new ArrayList[n + 1];
+        this.tree = new ArrayList[n + 1]; // 10
     }
 }

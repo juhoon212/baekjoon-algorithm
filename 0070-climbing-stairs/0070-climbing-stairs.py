17e0d3015172bdf61c0977778e494cd0a4ad096d
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-02-18 08:48:13 +0900

    Time: 0 ms (100%), Space: 12.4 MB (81.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,14 +5,20 @@ class Solution(object):
         :rtype: int
         """
         memo = [0] * 46
-        def cs(x):
-            if x == 1:
-                return 1
-            if x == 2:
-                return 2
 
-            if x not in memo:
-                memo[x] = cs(x-1) + cs(x-2)
-            return memo[x]
+        if n == 1:
+            return 1
+
+        if n == 2:
+            return 2
+
+        memo[1] = 1
+        memo[2] = 2
+        
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        return memo[i]
             
-        return cs(n)
+            
+                

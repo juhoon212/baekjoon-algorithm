bedea5b647897bb46254bb3bb8e042a488cbf4b7
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-02-24 08:26:00 +0900

    Time: 7 ms (28.25%), Space: 12.7 MB (30.25%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -9,6 +9,11 @@ class Solution(object):
         memo[0] = cost[0]
         memo[1] = cost[1]
 
-        for i in range(2, memo_length-1):
-            memo[i] = min(memo[i-1], memo[i-2]) + cost[i]
+        for i in range(2, len(cost)):
+            memo[i] = min(memo[i-1] + cost[i], memo[i-2] + cost[i])
+        
         return min(memo[len(cost)-1], memo[len(cost)-2])
+        
+
+
+        

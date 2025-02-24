7680e2c93650c99d569cdd45c96ab9ab5398cca9
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-02-25 08:55:19 +0900

    Time: 3 ms (73.88%), Space: 12.7 MB (30.18%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        memo_length = len(cost)+1
-        memo = [0] * memo_length
-        memo[0] = cost[0]
-        memo[1] = cost[1]
+        # 만약 두번째 index까지 올라간다 했을 때
+        # min(index-1, index) 중에 최소를 구하면 된다.
+        # 그러면 새로운 list를 만들어서 index-1 의 합을 저장해보자
+
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
 
         for i in range(2, len(cost)):
-            memo[i] = min(memo[i-1] + cost[i], memo[i-2] + cost[i])
-        
-        return min(memo[len(cost)-1], memo[len(cost)-2])
-        
-
-
-        
+            dp[i] = min(dp[i-1]+cost[i], dp[i-2]+cost[i])
+        return min(dp[len(cost)-1], dp[len(cost)-2])

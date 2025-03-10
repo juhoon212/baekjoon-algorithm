f386f40cebbc4170ca17ba831e09bf8aaf94863b
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-03-10 22:15:24 +0900

    Time: 0 ms (100%), Space: 17.8 MB (30.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,18 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def climbStairs(self, n: int) -> int:
         memo = [0] * 46
 
         if n == 1:
             return 1
-
+        
         if n == 2:
             return 2
 
         memo[1] = 1
         memo[2] = 2
-        
+
         for i in range(3, n+1):
             memo[i] = memo[i-1] + memo[i-2]
         
-        return memo[i]
-            
-            
-                
+        return memo[n]
+        

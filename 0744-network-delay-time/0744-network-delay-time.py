b062679efda7e5f5407df45b2cedf7fb4c2745b1
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-03-14 11:30:07 +0900

    Time: 423 ms (12.48%), Space: 19.7 MB (56.58%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,30 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # 최단거리 다익스트라
-        graph = collections.defaultdict(list) # 그래프 초기화
-
+        graph = collections.defaultdict(list) # 이중 그래프를 만들고
+        
         for time in times:
-            graph[time[0]].append((time[2], time[1])) # 이중 linkedList 구현 ex)  {2 : [(1, 1)]}
-
+            graph[time[0]].append((time[2], time[1]))
+        
         pq = []
         costs = {}
-
+        
         heapq.heappush(pq, (0, k))
 
         while pq:
-            cur_cost, cur_node = heapq.heappop(pq)
+            cur_v, cur_node = heapq.heappop(pq)
             if cur_node not in costs:
-                costs[cur_node] = cur_cost
-                for cost, next_node in graph[cur_node]:
-                    sum_cost = cost + cur_cost
+                costs[cur_node] = cur_v
+                for next_cost, next_node in graph[cur_node]:
+                    sum_cost = cur_v + next_cost
                     heapq.heappush(pq, (sum_cost, next_node))
         
+        # 최소 비용중에 최고 최소비용 (signal을 보낼 수 있는 최소 비용)
         for i in range(1, n+1):
             if i not in costs:
                 return -1
-
-        return max(costs.values())
-
-                
-
         
+        return max(costs.values())

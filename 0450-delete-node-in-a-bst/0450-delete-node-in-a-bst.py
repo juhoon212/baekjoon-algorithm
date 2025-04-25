83be4ec613c6c010061b4e3bb4d9d15d47edb65f
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-04-26 00:16:51 +0900

    Time: 0 ms (100%), Space: 21 MB (99.97%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,27 +8,30 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-        
+        # root.val > key -> 왼쪽
         if root.val > key:
             root.left = self.deleteNode(root.left, key)
+        # root.val < key -> 오른쪽
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
-        else :
-            if not root.left and not root.right:
+        # 같으면 자식들 탐방
+        else:
+            # 리프노드
+            if not root.right and not root.left:
                 return None
             
-            # 자식이 하나
+            # 자식하나
             if not root.left:
                 return root.right
             elif not root.right:
                 return root.left
             
-            # 완전 이진 트리
+            # 자식이 둘
             def findNode(node: TreeNode) -> TreeNode:
                 while node.left:
                     node = node.left
                 return node
-
+            # 오른쪽 자식중에 제일 작은 애를 root의 val로 만든다.
             result = findNode(root.right)
             root.val = result.val
             root.right = self.deleteNode(root.right, result.val)

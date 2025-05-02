8780af6af2b6eea977464bb1da870fae45ab06d7
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-05-02 22:06:27 +0900

    Time: 8 ms (38.8%), Space: 46.2 MB (25.82%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int m = maze.length, n = maze[0].length;
+        int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{entrance[0], entrance[1], 0});
+
+        boolean[][] visited = new boolean[m][n];
+        visited[entrance[0]][entrance[1]] = true;
+
+        while (!q.isEmpty()) {
+            int[] currPos = q.poll();
+            int currRow = currPos[0], currCol = currPos[1], currSteps = currPos[2];
+
+            for (int[] d : direction) {
+                int nextRow = currRow + d[0];
+                int nextCol = currCol + d[1];
+
+                if (nextRow >= 0 && nextCol >= 0 && nextRow < m && nextCol < n && 
+				    maze[nextRow][nextCol] == '.' && !visited[nextRow][nextCol] ) {
+
+                    if (nextRow == 0 || nextCol == 0 || nextRow == m - 1 || nextCol == n - 1) {
+                        return currSteps + 1;
+                    }
+
+                    visited[nextRow][nextCol] = true;
+                    q.offer(new int[]{nextRow, nextCol, currSteps + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+// TC: O(m * n)
+// SC: O(m * n) + O(m * n) => O(m * n)

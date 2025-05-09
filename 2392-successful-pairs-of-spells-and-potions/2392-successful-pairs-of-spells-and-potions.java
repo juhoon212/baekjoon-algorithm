7ffa90518afe1d439d785809ae66c5a6dbf7a470
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-05-09 21:10:01 +0900

    Time: 55 ms (8.83%), Space: 61.2 MB (97.87%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        // 가운데를 기준으로 기준 값 보다 클때
-        Arrays.sort(potions);
-
         List<Integer> list = new ArrayList<>();
+        Arrays.sort(potions);
 
         for (int spell : spells) {
             int left = 0, right = potions.length-1;
             int idx = potions.length;
             while (left <= right) {
                 int mid = (left + right) / 2;
-                if ((long)spell * potions[mid] >= success) {
+                
+                if ((long) spell * potions[mid] >= success) {
                     right = mid-1;
                     idx = mid;
                 } else {
                     left = mid+1;
                 }
             }
-
             list.add(potions.length - idx);
         }
-        
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }

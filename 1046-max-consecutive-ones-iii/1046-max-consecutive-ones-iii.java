40113378de2edd3febadde0102a7cb5e57bca3d9
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-05-25 12:00:58 +0900

    Time: 3 ms (91.84%), Space: 46.3 MB (82.08%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,22 +1,29 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        // sliding window
-        int zeroCount = 0;
-        int left = 0;
+        // nums 배열을 돌면서
+        // left, right 설정
+        // right는 하나씩 이동하면서 0을 만나면 zeroCount 하나씩 올림
+        // zeroCount가 k보다 많아질 시에는 left를 한칸씩 이동
+        // 언제까지? nums[left]가 0을 만나서 zeroCount가 k 보다 작아질때까지
+        // right - left + 1 -> 현재 최대 return 값
+        
+        int numsLength = nums.length;
         int max = 0;
+        int left = 0;
+        int zeroCount = 0;
 
-        for (int i=0; i<nums.length; ++i) {
-            if (nums[i] == 0) zeroCount++;
-
+        for (int r=0; r<numsLength; ++r) {
+            if (nums[r] == 0) zeroCount++;
+            
             while (zeroCount > k) {
                 if (nums[left] == 0) zeroCount--;
                 left++;
             }
 
-            int w = i - left + 1;
-            max = Math.max(max, w);
+            int currentLength = r - left + 1;
+            max = Math.max(max, currentLength);
         }
 
-        return max;
+        return max;        
     }
 }

cfd209fe2a3153450feaf720578fd7e0ad002fe7
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-05-29 22:58:49 +0900

    Time: 0 ms (100%), Space: 41 MB (79.18%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> answers = new ArrayList<>();  
-        backTrack(answers, new ArrayList<Integer>(), k, 1, n); 
+        List<List<Integer>> answers = new ArrayList<>();
+        backTrack(1, k, n, new ArrayList<>(), answers);
         return answers;
-    }
+    }   
 
-    void backTrack(
-        List<List<Integer>> answers, 
-        List<Integer> answer, 
-        int k, 
-        int start, 
-        int n
-    ) {
+    void backTrack(int idx, int k, int n, List<Integer> answer, List<List<Integer>> answers) {
         if (answer.size() == k && n == 0) {
             List<Integer> temp = new ArrayList<>(answer);
             answers.add(temp);
             return;
         }
 
-        for (int i=start; i<=9; ++i) {
+        for (int i=idx; i<=9; ++i) {
+            // 뭘 할거야?
             answer.add(i);
-            backTrack(answers, answer, k, i+1, n-i);
-            answer.remove(answer.size()-1);
+            backTrack(i+1, k, n-i, answer, answers);
+            answer.remove(answer.size() - 1);
         }
     }
 }

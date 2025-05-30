8a220821020a189b23ad10b4a0e04d39b7a85890
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-05-30 22:14:24 +0900

    Time: 0 ms (100%), Space: 41.1 MB (44.46%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,22 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
+        // k 개의 개수가 됬을 때 n이 되면 list에 담는다.
         List<List<Integer>> answers = new ArrayList<>();
         backTrack(1, k, n, new ArrayList<>(), answers);
         return answers;
     }   
-
-    void backTrack(int idx, int k, int n, List<Integer> answer, List<List<Integer>> answers) {
+    void backTrack(
+        int start, 
+        int k, 
+        int n, 
+        List<Integer> answer, 
+        List<List<Integer>> answers
+    ) {
+        // 탈출 조건
         if (answer.size() == k && n == 0) {
             List<Integer> temp = new ArrayList<>(answer);
             answers.add(temp);
             return;
         }
 
-        for (int i=idx; i<=9; ++i) {
-            // 뭘 할거야?
+        for (int i=start; i<=9; ++i) {
             answer.add(i);
             backTrack(i+1, k, n-i, answer, answers);
-            answer.remove(answer.size() - 1);
-        }
+            answer.remove(answer.size()-1);
+        }    
     }
 }

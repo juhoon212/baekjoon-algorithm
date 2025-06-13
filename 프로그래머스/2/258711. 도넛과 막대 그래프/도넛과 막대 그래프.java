88fe778ba2131fefc152c3789590a26936fea027
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-06-13 22:34:52 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 364.42 ms, Memory: 310 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,43 +2,48 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] edges) {
-        int [] answer = new int[4];
-        HashMap <Integer, int[]> graph = new HashMap<>();
-        int maxNode = -1;
+        HashMap<Integer, int[]> graph = new HashMap<>();
         
-        for(int [] now : edges) {
-            int from = now[0];
-            int to = now[1];
+        // 최대 값을 왜 구하는거지?
+        int maxValue = -1;
+        for (int[] edge: edges) {
+            int from = edge[0];
+            int to = edge[1];
             graph.putIfAbsent(from, new int[2]);
             graph.putIfAbsent(to, new int[2]);
+            
             graph.get(from)[0]++;
             graph.get(to)[1]++;
-            maxNode = Math.max(maxNode, Math.max(from, to));
+            
+            maxValue = Math.max(maxValue, Math.max(from, to));
         }
         
-        for(int i=1;i<=maxNode; i++){
+        int[] answer = new int[4];
+        
+        for (int i=1; i<=maxValue; ++i) {
             if (!graph.containsKey(i)) {
                 continue;
             }
             
-            int [] now = graph.get(i);
+            int[] now = graph.get(i);
             
-            //생성점 구하기
+            // 생성점
             if (now[0] >= 2 && now[1] == 0) {
-                answer[0]=i;
+                answer[0] = i;
             }
-            //막대 그래프 구하기
-            else if(now[0]==0) {
+            
+            // 막대 모양 그래프
+            if (now[0] == 0) {
                 answer[2]++;
             }
-            //8자 그래프 구하기
-            else if (now[0] >= 2 && now[1] >= 2) {
+            
+            if (now[0] >= 2 && now[1] >= 2) {
                 answer[3]++;
             }
         }
         
-        answer[1] = graph.get(answer[0])[0] - (answer[2]+answer[3]);
-         
+        answer[1] = graph.get(answer[0])[0] - (answer[2] + answer[3]);
+        
         return answer;
     }
 }

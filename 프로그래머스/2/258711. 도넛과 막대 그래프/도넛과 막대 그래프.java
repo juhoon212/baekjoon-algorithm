fc77fd969bdd978f856d9539ccdd6c123a4da36c
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-06-17 23:46:11 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 349.21 ms, Memory: 325 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -3,16 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         HashMap<Integer, int[]> graph = new HashMap<>();
-        
-        // 최대 값을 왜 구하는거지?
         int maxValue = -1;
-        for (int[] edge: edges) {
+        for (int[] edge : edges) {
             int from = edge[0];
             int to = edge[1];
+            
             graph.putIfAbsent(from, new int[2]);
             graph.putIfAbsent(to, new int[2]);
             
-            graph.get(from)[0]++;
+            graph.get(from)[0]++; // int 배열의 첫번째 인자는 node로 부터 나가는 간선
             graph.get(to)[1]++;
             
             maxValue = Math.max(maxValue, Math.max(from, to));
@@ -21,29 +20,28 @@ class Solution {
         int[] answer = new int[4];
         
         for (int i=1; i<=maxValue; ++i) {
-            if (!graph.containsKey(i)) {
-                continue;
-            }
+            if (!graph.containsKey(i)) continue;
+            
             
             int[] now = graph.get(i);
             
-            // 생성점
-            if (now[0] >= 2 && now[1] == 0) {
+            // 정점
+            if (now[0] >=2 && now[1] == 0) {
                 answer[0] = i;
             }
             
-            // 막대 모양 그래프
+            // 막대모양
             if (now[0] == 0) {
                 answer[2]++;
             }
             
+            // 8자
             if (now[0] >= 2 && now[1] >= 2) {
                 answer[3]++;
             }
         }
         
         answer[1] = graph.get(answer[0])[0] - (answer[2] + answer[3]);
-        
         return answer;
     }
 }

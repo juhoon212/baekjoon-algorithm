7cb938f76a3a0d6ebc20e4c93f58e0083931038e
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-06-18 22:24:47 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 347.01 ms, Memory: 307 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         HashMap<Integer, int[]> graph = new HashMap<>();
+        
         int maxValue = -1;
         for (int[] edge : edges) {
             int from = edge[0];
@@ -11,7 +12,7 @@ class Solution {
             graph.putIfAbsent(from, new int[2]);
             graph.putIfAbsent(to, new int[2]);
             
-            graph.get(from)[0]++; // int 배열의 첫번째 인자는 node로 부터 나가는 간선
+            graph.get(from)[0]++;
             graph.get(to)[1]++;
             
             maxValue = Math.max(maxValue, Math.max(from, to));
@@ -22,11 +23,10 @@ class Solution {
         for (int i=1; i<=maxValue; ++i) {
             if (!graph.containsKey(i)) continue;
             
-            
             int[] now = graph.get(i);
             
             // 정점
-            if (now[0] >=2 && now[1] == 0) {
+            if (now[0] >= 2 && now[1] == 0) {
                 answer[0] = i;
             }
             
@@ -35,13 +35,18 @@ class Solution {
                 answer[2]++;
             }
             
-            // 8자
+            // 8자 모양
             if (now[0] >= 2 && now[1] >= 2) {
                 answer[3]++;
             }
         }
         
+        // 도넛모양
+        // 정점에서 from이 총 그래프 수니까
+        // 정점 from - (막대모양 그래프 수 + 8자 모양 그래프 수)
+        
         answer[1] = graph.get(answer[0])[0] - (answer[2] + answer[3]);
+        
         return answer;
     }
 }

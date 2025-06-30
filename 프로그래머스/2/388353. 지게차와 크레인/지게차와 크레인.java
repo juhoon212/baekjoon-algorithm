d06fbebd4833371f08a8a2f47d5dbe2e814cdd01
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-06-30 20:10:55 +0900

    [level 2] Title: 지게차와 크레인, Time: 91.10 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,91 +1,92 @@
 import java.util.*;
 
 class Solution {
-    static final int INF = 10001;
-    static int [] arx = {-1,1,0,0};
-    static int [] ary = {0,0,-1,1};
-    static int n, m;
+    int[] dx = new int[]{0, -1, 1, 0};
+    int[] dy = new int[]{1, 0, 0, -1};
+    final int INF = 10001;
+    int answer;
     public int solution(String[] storage, String[] requests) {
-        int answer = 0;
-        n = storage.length;       // 행
-        m = storage[0].length();  // 열
-        int [][] map = new int [n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                map[i][j] = storage[i].charAt(j) - 'A';
+        int n = storage.length;
+        int m = storage[0].length();
+        // 1. 먼저 2차원 배열을 만들고 각 알파벳 세팅
+        int[][] wareHouse = new int[n][m];
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<m; ++j) {
+                wareHouse[i][j] = storage[i].charAt(j) - 'A';
             }
         }
-        // 요청 처리 
-        for(String request : requests){
+        // requests를 돌면서  
+        for (String request : requests) {
             int target = request.charAt(0) - 'A';
-            // 크레인 로직 
-            if(request.length() == 2){
-                for(int i = 0; i < n; i++){
-                    for(int j = 0; j < m; j++){
-                        if(map[i][j] == target){
-                            map[i][j] = -1;
-                        }
+            // requests의 알파벳 길이가 2이면 크레인 이용
+            if (request.length() == 2) {
+                for (int i=0; i<n; ++i) {
+                    for (int j=0; j<m; ++j) {
+                        if (wareHouse[i][j] == target) wareHouse[i][j] = -1;
                     }
                 }
-                continue;
             }
-            // 지게차 로직
-            int [][] memo = new int [n][m];
-            for(int i = 0; i < n; i++){
+            
+            // 1이면 지게차 이용
+            // 지게차는 가장 자리에 있는 것만 삭제 가능
+            int[][] memo = new int[n][m];
+            for (int i=0; i<n; ++i) {
                 Arrays.fill(memo[i], INF);
             }
-            ArrayList<int[]> list = new ArrayList<>();
-            Queue<int[]> q = new LinkedList<>();
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(i != 0 && j != 0 && i != n - 1 && j != m - 1) continue;
-                    if(memo[i][j] != INF) continue;
-                    if(map[i][j] != -1){
+            List<int[]> list = new ArrayList<>(); // 제거대상
+            Queue<int[]> q = new ArrayDeque<>();
+            
+            for (int i=0; i<n; ++i) {
+                for (int j=0; j<m; ++j) {
+                    if (i != 0 && j != 0 && i != n-1 && j != m-1) continue;
+                    //if (memo[i][j] != INF) continue;
+                    if (wareHouse[i][j] != -1) {
                         memo[i][j] = 1;
-                        if(map[i][j] == target){
-                            list.add(new int []{i,j});
-                        }
+                        
+                        if (wareHouse[i][j] == target) list.add(new int[]{i, j});
                         continue;
                     }
+                    
                     memo[i][j] = 0;
-                    q.add(new int [] {i, j});
-                    while(!q.isEmpty()){
-                        int [] cur = q.poll();
-                        for(int k = 0; k < 4; k++){
-                            int nx = cur[0] + arx[k];
-                            int ny = cur[1] + ary[k];
-                            if(!validation(nx, ny) || memo[nx][ny] != INF) continue;
-                            if(map[nx][ny] == -1){
-                                memo[nx][ny] = 0;
-                                q.add(new int [] {nx, ny});
+                    q.add(new int[]{i, j});
+                    
+                    while (!q.isEmpty()) {
+                        int[] now = q.poll();
+                        
+                        for (int k=0; k<4; ++k) {
+                            int nowX = now[0] + dx[k];
+                            int nowY = now[1] + dy[k];
+                            if (!validation(nowX, nowY, n, m) || memo[nowX][nowY] != INF) continue;
+                            if (wareHouse[nowX][nowY] == -1) {
+                                memo[nowX][nowY] = 0;
+                                q.add(new int[]{nowX, nowY});
                                 continue;
                             }
-                            memo[nx][ny] = 1;
-                            if(map[nx][ny] == target){
-                                list.add(new int []{nx,ny});
+                            memo[nowX][nowY] = 1;
+                            if (wareHouse[nowX][nowY] == target) {
+                                list.add(new int[]{nowX, nowY});
                             }
                         }
                     }
                 }
-            }   
-            // 이번 요청에서 제거 대상인 컨테이너 제거 
-            for(int [] next : list){
-                map[next[0]][next[1]] = -1;
+            }
+            
+            for (int[] next : list) {
+                wareHouse[next[0]][next[1]] = -1;
+            }
+        }
+            
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<m; ++j) {
+                if (wareHouse[i][j] != -1) answer++;
             }
         }
         
-        // 남은 컨테이너 개수 count
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(map[i][j] != -1) answer++;
-            }
-        }
         return answer;
     }
     
-    // Arrays out of index 방지 
-    public static boolean validation(int nx, int ny){
-        if(0 <= nx && 0 <= ny && nx < n && ny < m) return true;
-        return false;
+    boolean validation(int nowX, int nowY, int n, int m) {
+        if (nowX < 0 || nowX >= n || nowY < 0 || nowY >= m) return false;
+        return true;
     }
 }

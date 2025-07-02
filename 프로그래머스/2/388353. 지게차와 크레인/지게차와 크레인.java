019b28605f57815c3f403ab01610035947f53d1e
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-02 21:34:18 +0900

    [level 2] Title: 지게차와 크레인, Time: 94.17 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -3,90 +3,93 @@ import java.util.*;
 class Solution {
     int[] dx = new int[]{0, -1, 1, 0};
     int[] dy = new int[]{1, 0, 0, -1};
-    final int INF = 10001;
     int answer;
+    int INF = 1001;
     public int solution(String[] storage, String[] requests) {
+        // 먼저 2차원 배열 생성
         int n = storage.length;
         int m = storage[0].length();
-        // 1. 먼저 2차원 배열을 만들고 각 알파벳 세팅
-        int[][] wareHouse = new int[n][m];
+        int[][] map = new int[n][m];
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
-                wareHouse[i][j] = storage[i].charAt(j) - 'A';
+                map[i][j] = storage[i].charAt(j) - 'A';
             }
         }
-        // requests를 돌면서  
+        
         for (String request : requests) {
+            // 크레인
             int target = request.charAt(0) - 'A';
-            // requests의 알파벳 길이가 2이면 크레인 이용
+            
             if (request.length() == 2) {
                 for (int i=0; i<n; ++i) {
                     for (int j=0; j<m; ++j) {
-                        if (wareHouse[i][j] == target) wareHouse[i][j] = -1;
+                        if (map[i][j] == target) map[i][j] = -1;
                     }
-                }
+                }                
             }
             
-            // 1이면 지게차 이용
-            // 지게차는 가장 자리에 있는 것만 삭제 가능
+            // 지게차
             int[][] memo = new int[n][m];
+
+            // memo 배열에 int 값 삽입 -> 삭제됬는지 안됬는지 확인
             for (int i=0; i<n; ++i) {
                 Arrays.fill(memo[i], INF);
             }
-            List<int[]> list = new ArrayList<>(); // 제거대상
+            
+            List<int[]> list = new ArrayList<>();
             Queue<int[]> q = new ArrayDeque<>();
             
             for (int i=0; i<n; ++i) {
                 for (int j=0; j<m; ++j) {
-                    if (i != 0 && j != 0 && i != n-1 && j != m-1) continue;
-                    //if (memo[i][j] != INF) continue;
-                    if (wareHouse[i][j] != -1) {
+                    // 출발점 구하기
+                    if (i != 0 && j !=0 && i != n-1 && j != m-1) continue;
+                    if (map[i][j] != -1) {
                         memo[i][j] = 1;
-                        
-                        if (wareHouse[i][j] == target) list.add(new int[]{i, j});
+                        if (map[i][j] == target) list.add(new int[]{i, j});
                         continue;
                     }
                     
                     memo[i][j] = 0;
-                    q.add(new int[]{i, j});
+                    q.add(new int[]{i,j});
                     
                     while (!q.isEmpty()) {
                         int[] now = q.poll();
                         
                         for (int k=0; k<4; ++k) {
-                            int nowX = now[0] + dx[k];
-                            int nowY = now[1] + dy[k];
-                            if (!validation(nowX, nowY, n, m) || memo[nowX][nowY] != INF) continue;
-                            if (wareHouse[nowX][nowY] == -1) {
-                                memo[nowX][nowY] = 0;
-                                q.add(new int[]{nowX, nowY});
+                            int nextX = now[0] + dx[k];
+                            int nextY = now[1] + dy[k];
+                            
+                            if (!validate(n, m, nextX, nextY) || memo[nextX][nextY] != INF) continue;
+                            if (map[nextX][nextY] == -1) {
+                                memo[nextX][nextY] = 0;
+                                q.add(new int[]{nextX, nextY});
                                 continue;
                             }
-                            memo[nowX][nowY] = 1;
-                            if (wareHouse[nowX][nowY] == target) {
-                                list.add(new int[]{nowX, nowY});
+                            memo[nextX][nextY] = 1;
+                            if (map[nextX][nextY] == target) {
+                                list.add(new int[]{nextX, nextY});
                             }
                         }
                     }
                 }
             }
             
-            for (int[] next : list) {
-                wareHouse[next[0]][next[1]] = -1;
+            for (int[] candidate : list) {
+                map[candidate[0]][candidate[1]] = -1;
             }
         }
-            
+        
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
-                if (wareHouse[i][j] != -1) answer++;
+                if (map[i][j] != -1) answer++;
             }
         }
         
         return answer;
     }
     
-    boolean validation(int nowX, int nowY, int n, int m) {
-        if (nowX < 0 || nowX >= n || nowY < 0 || nowY >= m) return false;
+    boolean validate(int n, int m, int nextX, int nextY) {
+        if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) return false;
         return true;
     }
 }

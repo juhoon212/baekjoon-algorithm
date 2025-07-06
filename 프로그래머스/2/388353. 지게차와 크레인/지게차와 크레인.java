2b5bcb3098c756393bd0916fcb7bc25dfc5eea44
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-06 10:06:24 +0900

    [level 2] Title: 지게차와 크레인, Time: 127.07 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,70 +1,67 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = new int[]{0, -1, 0, 1};
-    int[] dy = new int[]{1, 0, -1, 0};
+    int[] dx = new int[]{0, -1, 1, 0};
+    int[] dy = new int[]{1, 0, 0, -1};
     int answer;
+    final int INF = 1001;
     public int solution(String[] storage, String[] requests) {
+        // 2차원 배열 생성
         int n = storage.length;
         int m = storage[0].length();
-        final int INF = 10001;
         
-        // 2차원 배열 생성
         int[][] map = new int[n][m];
+        
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 map[i][j] = storage[i].charAt(j) - 'A';
-            } 
+            }
         }
         
+        // target 마다 지게차, 크레인 로직 실행
         for (String request : requests) {
             int target = request.charAt(0) - 'A';
+            
             // 크레인
             if (request.length() == 2) {
                 for (int i=0; i<n; ++i) {
                     for (int j=0; j<m; ++j) {
-                        // 삭제 처리
                         if (map[i][j] == target) map[i][j] = -1;
+                        continue;
                     }
                 }
             }
-            
-            // request마다 memo 배열을 생성
+            // 지게차
             int[][] memo = new int[n][m];
-            
             for (int i=0; i<n; ++i) {
-                // 방문 x, 삭제된지도 모르면 INF
                 Arrays.fill(memo[i], INF);
             }
             
-            // 삭제 예정 리스트
             List<int[]> list = new ArrayList<>();
             Queue<int[]> q = new ArrayDeque<>();
             
             for (int i=0; i<n; ++i) {
                 for (int j=0; j<m; ++j) {
-                    // 출발점 구하기 - 가장자리가 아니면 pass
+                    // 출발점 찾기
                     if (i != 0 && j != 0 && i < n-1 && j < m-1) continue;
-                    // 삭제되지 않은 지점이라면
                     if (map[i][j] != -1) {
-                        // 방문했다.
                         memo[i][j] = 1;
                         if (map[i][j] == target) list.add(new int[]{i, j});
                         continue;
                     }
                     
-                    // 미방문
+                    // 내부 
                     memo[i][j] = 0;
                     q.add(new int[]{i, j});
                     
                     while (!q.isEmpty()) {
-                        int now[] = q.poll();
+                        int[] now = q.poll();
                         
                         for (int k=0; k<4; ++k) {
                             int nextX = now[0] + dx[k];
                             int nextY = now[1] + dy[k];
                             
-                            if (!validate(n, m, nextX, nextY) || memo[nextX][nextY] != INF) continue;
+                            if (!validate(nextX, nextY, n, m) || memo[nextX][nextY] != INF) continue;
                             if (map[nextX][nextY] == -1) {
                                 memo[nextX][nextY] = 0;
                                 q.add(new int[]{nextX, nextY});
@@ -78,7 +75,7 @@ class Solution {
                 }
             }
             
-            for (int[] now : list) {
+            for (int[] now: list) {
                 map[now[0]][now[1]] = -1;
             }
         }
@@ -92,7 +89,8 @@ class Solution {
         return answer;
     }
     
-        boolean validate(int n, int m, int x, int y) {
-        return (0 <= x && x < n && 0 <= y && y < m);
+    boolean validate(int x, int y, int n, int m) {
+        if (x < 0 || y < 0 || x > n-1 || y > m-1) return false;
+        return true;
     }
 }

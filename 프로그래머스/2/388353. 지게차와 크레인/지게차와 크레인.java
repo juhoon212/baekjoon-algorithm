5344e4175293fd4cb12d48d34fd9f50051c412d2
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-07 21:32:39 +0900

    [level 2] Title: 지게차와 크레인, Time: 130.20 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,24 +1,22 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = new int[]{0, -1, 1, 0};
+    int[] dx = new int[]{0, 1, -1, 0};
     int[] dy = new int[]{1, 0, 0, -1};
     int answer;
-    final int INF = 1001;
     public int solution(String[] storage, String[] requests) {
         // 2차원 배열 생성
         int n = storage.length;
         int m = storage[0].length();
-        
+        final int INF = 100001;
         int[][] map = new int[n][m];
-        
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 map[i][j] = storage[i].charAt(j) - 'A';
             }
         }
         
-        // target 마다 지게차, 크레인 로직 실행
+        
         for (String request : requests) {
             int target = request.charAt(0) - 'A';
             
@@ -26,13 +24,15 @@ class Solution {
             if (request.length() == 2) {
                 for (int i=0; i<n; ++i) {
                     for (int j=0; j<m; ++j) {
-                        if (map[i][j] == target) map[i][j] = -1;
+                        if (map[i][j] == target) map[i][j] = -1; 
                         continue;
                     }
                 }
             }
+            
             // 지게차
             int[][] memo = new int[n][m];
+            // INF는 한번도 탐색하지 않은 곳
             for (int i=0; i<n; ++i) {
                 Arrays.fill(memo[i], INF);
             }
@@ -44,42 +44,41 @@ class Solution {
                 for (int j=0; j<m; ++j) {
                     // 출발점 찾기
                     if (i != 0 && j != 0 && i < n-1 && j < m-1) continue;
+                    // 삭제 되지 않은 노드라면
                     if (map[i][j] != -1) {
-                        memo[i][j] = 1;
+                        memo[i][j] = 1; // 외부
                         if (map[i][j] == target) list.add(new int[]{i, j});
                         continue;
                     }
-                    
-                    // 내부 
-                    memo[i][j] = 0;
-                    q.add(new int[]{i, j});
-                    
+                    // 나머지는 내부
+                    memo[i][j] = 1;
+                    q.add(new int[]{i,j});
+
                     while (!q.isEmpty()) {
                         int[] now = q.poll();
-                        
+
                         for (int k=0; k<4; ++k) {
-                            int nextX = now[0] + dx[k];
-                            int nextY = now[1] + dy[k];
-                            
-                            if (!validate(nextX, nextY, n, m) || memo[nextX][nextY] != INF) continue;
-                            if (map[nextX][nextY] == -1) {
-                                memo[nextX][nextY] = 0;
-                                q.add(new int[]{nextX, nextY});
+                            int nowX = now[0] + dx[k];
+                            int nowY = now[1] + dy[k];
+
+                            if (!validate(n, m, nowX, nowY) || memo[nowX][nowY] != INF) continue;
+                            if (map[nowX][nowY] == -1) {
+                                memo[nowX][nowY] = 0;
+                                q.add(new int[]{nowX, nowY});
                                 continue;
                             }
-                            
-                            memo[nextX][nextY] = 1;
-                            if (map[nextX][nextY] == target) list.add(new int[]{nextX, nextY});
+
+                            memo[nowX][nowY] = 1;
+                            if (map[nowX][nowY] == target) list.add(new int[]{nowX, nowY});
                         }
                     }
                 }
             }
             
-            for (int[] now: list) {
-                map[now[0]][now[1]] = -1;
+            for (int[] candidate : list) {
+                map[candidate[0]][candidate[1]] = -1;
             }
         }
-        
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 if (map[i][j] != -1) answer++;
@@ -89,7 +88,7 @@ class Solution {
         return answer;
     }
     
-    boolean validate(int x, int y, int n, int m) {
+    boolean validate(int n, int m, int x, int y) {
         if (x < 0 || y < 0 || x > n-1 || y > m-1) return false;
         return true;
     }

1d0971d8bf006c5af8b7f2aa50338ab694780301
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-13 09:34:48 +0900

    [level 2] Title: 지게차와 크레인, Time: 45.57 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,42 +1,49 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = new int[]{0, -1, 1, 0};
-    int[] dy = new int[]{1, 0, 0, -1};
-
     public int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        
+        // 대문자 2차원 배열 생성
         int n = storage.length;
         int m = storage[0].length();
-        final int INF = 100000001;
-        int answer = 0;
-
+        
+        int[] dx = new int[]{0, -1, 0, 1};
+        int[] dy = new int[]{-1, 0, 1, 0};
+        
         int[][] map = new int[n][m];
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
+        
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<m; ++j) {
                 map[i][j] = storage[i].charAt(j) - 'A';
-
+            }
+        }
+        
         for (String request : requests) {
             int target = request.charAt(0) - 'A';
-
-            // 지게차 요청
+            
+            // 크레인
             if (request.length() == 2) {
-                for (int i = 0; i < n; ++i)
-                    for (int j = 0; j < m; ++j)
-                        if (map[i][j] == target)
-                            map[i][j] = -1;
-                continue;
+                for (int i=0; i<n; ++i) {
+                    for (int j=0; j<m; ++j) {
+                        if (map[i][j] == target) map[i][j] = -1;
+                        continue;
+                    } 
+                }
             }
-
-            // 크레인 요청
+            
+            // 지게차
             int[][] memo = new int[n][m];
+            final int INF = 10000001;
+            for (int i=0; i<n; ++i) {
+                Arrays.fill(memo[i], INF);
+            }
+            
             Queue<int[]> q = new ArrayDeque<>();
-            for (int i = 0; i < n; ++i) Arrays.fill(memo[i], INF);
-
-            // 1. 가장자리에서 BFS 시작점 수집
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < m; ++j) {
-                    if (!(i == 0 || i == n - 1 || j == 0 || j == m - 1)) continue;
-
+            
+            for (int i=0; i<n; ++i) {
+                for (int j=0; j<m; ++j) {
+                    if (i != 0 && j != 0 && i < n-1 && j < m-1) continue;
                     if (map[i][j] == -1) {
                         memo[i][j] = 0;
                         q.add(new int[]{i, j});
@@ -46,28 +53,27 @@ class Solution {
                     }
                 }
             }
-
-            // 2. BFS 단 한 번 수행
+            
             while (!q.isEmpty()) {
                 int[] now = q.poll();
-                for (int k = 0; k < 4; ++k) {
-                    int nextX = now[0] + dx[k];
-                    int nextY = now[1] + dy[k];
+                
+                for (int i=0; i<4; ++i) {
+                    int nextX = now[0] + dx[i];
+                    int nextY = now[1] + dy[i];
+                    
                     if (!validate(n, m, nextX, nextY) || memo[nextX][nextY] != INF) continue;
-
+                    
                     if (map[nextX][nextY] == -1) {
                         memo[nextX][nextY] = 0;
                         q.add(new int[]{nextX, nextY});
                     } else {
                         memo[nextX][nextY] = 1;
-                        if (map[nextX][nextY] == target) {
-                            map[nextX][nextY] = -1;
-                        }
+                        if (map[nextX][nextY] == target) map[nextX][nextY] = -1;
                     }
                 }
             }
         }
-
+        
         // 살아남은 박스 개수 세기
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j)
@@ -75,8 +81,9 @@ class Solution {
 
         return answer;
     }
-
+    
     boolean validate(int n, int m, int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < m;
+        if (x < 0 || x > n-1 || y < 0 || y > m-1) return false;
+        return true;
     }
 }

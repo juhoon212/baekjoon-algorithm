3156adf358f12f4a41dbc7f8141916c44f777d23
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-15 23:32:37 +0900

    [level 2] Title: 서버 증설 횟수, Time: 3.49 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,26 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] players, int m, int k) {
-        int cnt = 0;
+        
+        int cnt = 0; // 증설 횟수
+        
         Queue<Integer> q = new ArrayDeque<>();
         
+        // 24시간 동안
         for (int i=0; i<24; ++i) {
             int need = players[i] / m;
             
-            while (!q.isEmpty() && q.peek() <= i) {
-                q.poll();
-            }
+            while (!q.isEmpty() && q.peek() <= i) q.poll();
             
             int current = q.size();
             
             if (need > current) {
-                int newServer = need - current;
-                cnt += newServer;
-                for (int j=0; j<newServer; ++j) {
+                int newServerNeed = need - current;
+                cnt += newServerNeed;
+                for (int j=0; j<newServerNeed; ++j) {
                     q.add(i + k);
                 }
             }
         }
+        
         return cnt;
     }
 }

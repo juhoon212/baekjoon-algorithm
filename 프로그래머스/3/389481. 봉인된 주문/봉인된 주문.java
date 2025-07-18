b87094ad7f984f3007f1a74bc6af3f83b6dfc9a3
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-18 23:56:10 +0900

    [level 3] Title: 봉인된 주문, Time: 268.20 ms, Memory: 207 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 class Solution {
     public String solution(long n, String[] bans) {
         String answer = "";
+        // 26진법으로 고쳐서 list에 담는다.
         List<Long> values = Arrays.stream(bans)
             .map(ban -> convertTo26(ban))
             .sorted()
@@ -24,6 +25,8 @@ class Solution {
         
         var num = target;
         
+        // 26으로 나누면서 그 나머지를 'a' 와 더해서 stringbuilder에 담고 계속해서 26으로 나눈다.
+        // 뒤에서 부터 계산 (target이 777이면 0부터 계산이 아니라 777부터 계산하니까) reverse() 써줘야함.
         while (num-- > 0) {
             int c = (int)('a' + (num % 26));
             sb.append((char) c);
@@ -33,6 +36,7 @@ class Solution {
         return sb.reverse().toString();
     }
     
+    // 26진법 변환
     Long convertTo26(String target) {
         // a~z = 26
         Long result = 0L;

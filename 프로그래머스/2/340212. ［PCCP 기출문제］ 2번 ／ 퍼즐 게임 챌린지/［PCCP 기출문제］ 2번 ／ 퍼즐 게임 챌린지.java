587c82750a8d06a4d53893e7bf70e06917fedf76
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-07-28 22:10:49 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 40.07 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,29 +1,32 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
 
-        return binary_search(diffs, times, limit);
-
+        return binarySearch(diffs, times, limit);
     }
-
-    public int binary_search(int [] diffs, int [] times, long limit) {
-        int max = 1; int min = 100000; 
-        while (max <= min){
-            int level = (max+min)/2; 
-            long mid = calcul(diffs,times, level);
-            // 제한 시간을 초과함 -> level 상승
-            if(mid > limit) max = level + 1; 
-            // 제한시간보다 빨리 끝내거나 딱 맞출 때 -> level 하락
-            else min = level - 1; 
+    
+    public int binarySearch(int[] diffs, int[] times, long limit) {
+        int max = 1;
+        int min = 100000;
+        
+        while (max <= min) {
+            int level = (min + max) / 2;        
+            long mid = calculate(diffs, times, level);
+            
+            if (mid > limit) max = level + 1;
+            else min = level - 1;
         }
+        
         return max;
     }
-
-    public long calcul(int [] diffs, int [] times, int level) {
-        long ans = 0;
-        for(int i = 0; i < diffs.length; i++){
-            if(diffs[i] <= level) ans += times[i];
-            else ans += (long)(times[i] + times[i-1])* (long)(diffs[i] - level) + times[i];
+    
+    public long calculate(int[] diffs, int[] times, int level) {
+        long cur = 0;
+        for (int i=0; i<diffs.length; ++i) {
+            if (diffs[i] <= level) cur += times[i];
+            else {
+                cur += (long)(times[i - 1] + times[i]) * (long)(diffs[i] - level) + times[i];
+            }
         }
-        return ans;
+        return cur;
     }
 }

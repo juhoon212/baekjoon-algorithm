4d7d950bc268b4e26bd0c1c379032cd0873f0cee
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-04 22:17:53 +0900

    [level 2] Title: 비밀 코드 해독, Time: 31.10 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,53 +1,45 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer>[] set;
-    boolean[] visited;
-    int[] answers = new int[5]; // 입력한 정수
+    int[] code = new int[5]; // 비밀 코드
     int answer;
+    Set<Integer>[] set;
     public int solution(int n, int[][] q, int[] ans) {
-        // 비밀 코드는 뭔지 모른다
-        // 하지만 입력한 정수와 비밀 코드와의 일치관계는 알 수 있다.
-        // q = 입력한 정수 배열
-        // ans = 일치 개수
+        // 1~n 
+        // m 번의 시도
+        // 정수 조합
         set = new HashSet[q.length];
-        visited = new boolean[n+1];
         for (int i=0; i<q.length; ++i) {
             set[i] = new HashSet<>();
             for (int j=0; j<q[0].length; ++j) {
                 set[i].add(q[i][j]);
             }
         }
-        
-        makeArr(0, 1, n, q, ans);
+        makeAnswer(0, 1, n, q, ans);
         return answer;
     }
     
-    void makeArr(int cnt, int cur, int n, int[][] q, int[] ans) {
+    void makeAnswer(int cnt, int cur, int n, int[][] q, int[] ans) {
         if (cnt == 5) {
             if (check(q, ans)) answer++;
             return;
         }
         
-        for (int i=cur; i<=n; ++i) {
-            if (visited[i]) continue;
-            answers[cnt] = i;
-            
-            visited[i] = true;
-            makeArr(cnt+1, i+1, n, q, ans);
-            visited[i] = false;
+        for (int i=cur; i<=n; ++i) {   
+            code[cnt] = i;
+            makeAnswer(cnt+1, i+1, n, q, ans);
         }
     }
     
-    boolean check(int[][] q, int[] ans) {
+    boolean check(int[][] q, int[] answer) {
         for (int i=0; i<q.length; ++i) {
-            int sum = 0;
+            int total = 0;
             
-            for (int a : answers) {
-                if (set[i].contains(a)) sum++;
+            for (int now : code) {
+                if (set[i].contains(now)) total++;
             }
             
-            if (sum != ans[i]) return false;
+            if (total != answer[i]) return false;
         }
         
         return true;

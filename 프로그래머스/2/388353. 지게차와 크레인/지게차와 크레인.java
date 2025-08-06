bcac25878e198b92450596c8f7ad8f49c4e97792
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-06 22:25:05 +0900

    [level 2] Title: 지게차와 크레인, Time: 53.04 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = new int[]{1, 0, -1, 0};
+    int[] dy = new int[]{0, -1, 0, 1};
+    
     public int solution(String[] storage, String[] requests) {
-        int answer = 0;
-        
-        // 대문자 2차원 배열 생성
         int n = storage.length;
         int m = storage[0].length();
         
-        int[] dx = new int[]{0, -1, 0, 1};
-        int[] dy = new int[]{-1, 0, 1, 0};
-        
         int[][] map = new int[n][m];
+        int result = 0;
         
+        // map 세팅
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 map[i][j] = storage[i].charAt(j) - 'A';
@@ -22,33 +21,33 @@ class Solution {
         for (String request : requests) {
             int target = request.charAt(0) - 'A';
             
+            // -1은 지워짐, 0은 
             // 크레인
             if (request.length() == 2) {
                 for (int i=0; i<n; ++i) {
                     for (int j=0; j<m; ++j) {
                         if (map[i][j] == target) map[i][j] = -1;
-                        continue;
-                    } 
+                    }
                 }
+                continue;
             }
             
-            // 지게차
             int[][] memo = new int[n][m];
+            // memo 배열 채우기
             final int INF = 10000001;
             for (int i=0; i<n; ++i) {
                 Arrays.fill(memo[i], INF);
             }
             
             Queue<int[]> q = new ArrayDeque<>();
-            
             for (int i=0; i<n; ++i) {
                 for (int j=0; j<m; ++j) {
-                    if (i != 0 && j != 0 && i < n-1 && j < m-1) continue;
+                    if (i != 0 && j != 0 && i<n-1 && j<m-1) continue;
                     if (map[i][j] == -1) {
                         memo[i][j] = 0;
                         q.add(new int[]{i, j});
                     } else {
-                        memo[i][j] = 1;
+                        memo[i][j] = 1; // 갈수 없는 공간
                         if (map[i][j] == target) map[i][j] = -1;
                     }
                 }
@@ -58,10 +57,12 @@ class Solution {
                 int[] now = q.poll();
                 
                 for (int i=0; i<4; ++i) {
-                    int nextX = now[0] + dx[i];
-                    int nextY = now[1] + dy[i];
+                    int nextX = dx[i] + now[0];
+                    int nextY = dy[i] + now[1];
                     
-                    if (!validate(n, m, nextX, nextY) || memo[nextX][nextY] != INF) continue;
+                    if (!validate(nextX, nextY, n, m) || memo[nextX][nextY] != INF) {
+                        continue;
+                    }
                     
                     if (map[nextX][nextY] == -1) {
                         memo[nextX][nextY] = 0;
@@ -74,16 +75,16 @@ class Solution {
             }
         }
         
-        // 살아남은 박스 개수 세기
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (map[i][j] != -1) answer++;
-
-        return answer;
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<m; ++j) {
+                if (map[i][j] != -1) result++;
+            }
+        }
+        
+        return result;
     }
     
-    boolean validate(int n, int m, int x, int y) {
-        if (x < 0 || x > n-1 || y < 0 || y > m-1) return false;
-        return true;
+    boolean validate(int x, int y, int n, int m) {
+        return !(x < 0 || y < 0 || x >= n || y >= m); 
     }
 }

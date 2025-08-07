3d3054350f800ff58bb8f72ddded8200bc5bb1cb
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-07 22:52:52 +0900

    [Gold IV] Title: N-Queen, Time: 6024 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,38 +2,45 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int[] answer;
     static int N;
-    static int[] arr;
-    static int answer = 0;
-    public static void main(String[] args) throws Exception{
+    static int result=0;
+    public static void main(String[] args) throws Exception {
+        // N만큼의 Queen을 배치해야한다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        init(Integer.parseInt(br.readLine()));
         
-        queen(0);
-        System.out.println(answer);
+        backTracking(0); // 조합
+        System.out.println(result);
     }
     
-    public static void queen(int depth) {
-        if (depth == N) {
-            answer++;
+    static void init(int n) {
+        N = n;
+        answer = new int[n];
+    }
+    
+    static void backTracking(int col) {
+        if (col == N) { // 모든 열을 다돌았다. 즉 모든 queen이 가질 수 있는 조합을 완성시켰다.
+            result++;
             return;
         }
         
-        // queen은 같은 행, 열, 대각선에 배치할 수 없다.
         for (int i=0; i<N; ++i) {
-            arr[depth] = i;
-            if (check(depth)) {
-                queen(depth + 1);
+            answer[col] = i;
+            
+            if (check(col)) {
+                backTracking(col+1);
             }
         }
     }
     
-    public static boolean check(int col) {
+    // Queen이 현재 같은 행에 있거나, 대각선에 있는지 확인
+    static boolean check(int col) {
         for (int i=0; i<col; ++i) {
-            if (arr[col] == arr[i]) return false;
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false;
+            if (answer[i] == answer[col]) return false;
+            if (Math.abs(i - col) == Math.abs(answer[i] - answer[col])) return false;
         }
+        
         return true;
     }
 }

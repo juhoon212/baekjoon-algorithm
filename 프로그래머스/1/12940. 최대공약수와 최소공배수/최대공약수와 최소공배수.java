d572f2cb6c34633475e654b254b499dd89d2137a
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-13 05:58:37 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[] solution(int n, int m) {
+        // [0] = 최대공약수, [1] = 최소 공배수
+        
         int gcd = GCD(n, m);
-        int lcm = LCM(n * m, gcd);
+        int lcm = LCM(gcd, n*m); // 최소공배수는 두수의 곱 / 최대공약수
         return new int[]{gcd, lcm};
     }
     
-    int GCD(int n, int m) {
-        if (m == 0) return n;
+    int GCD(int n, int m) { // 유클리드 호재법
+        if (m == 0) {
+            return n;
+        }
+        
         return GCD(m, n%m);
     }
     
-    int LCM(int multiple, int gcd) {
+    int LCM(int gcd, int multiple) {
         return multiple / gcd;
     }
 }

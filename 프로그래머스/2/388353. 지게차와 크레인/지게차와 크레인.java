fe7280d35ef9d6beab8f45a9596c0fa84f90998f
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-24 12:24:25 +0900

    [level 2] Title: 지게차와 크레인, Time: 28.21 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,44 +1,51 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = new int[]{0, -1, 0, 1};
-    int[] dy = new int[]{1, 0, -1, 0};
-    int answer;
+    int[] dx = new int[]{0, -1, 1, 0};
+    int[] dy = new int[]{1, 0, 0, -1};
     public int solution(String[] storage, String[] requests) {
         int n = storage.length;
         int m = storage[0].length();
-        // 1.map 생성 후 초기화
+        
         int[][] map = new int[n][m];
+        int answer = 0;
+        
+        // map 초기화
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 map[i][j] = storage[i].charAt(j) - 'A';
             }
         }
+        
         for (String request : requests) {
             int target = request.charAt(0) - 'A';
-            // 2.크레인 -> request의 길이가 2
+            // crane
             if (request.length() == 2) {
                 for (int i=0; i<n; ++i) {
                     for (int j=0; j<m; ++j) {
-                        // -1 은 삭제노드
+                        // 바로 삭제 처리
                         if (map[i][j] == target) map[i][j] = -1;
                     }
                 }
                 
                 continue;
             }
-            // 3.지게차 -> 길이가 1 visited 배열로 방문한 노드인지 판별
+            
+            // 지게차
+            // visited 배열 생성 - 지게차로 탐색 가능한지
             boolean[][] visited = new boolean[n][m];
             Queue<int[]> q = new ArrayDeque<>();
-            // 3-1. bfs 시작점 구하기 - 테두리에서만 시작해야함
+            
+            // 시작점 구하기
             for (int i=0; i<n; ++i) {
                 for (int j=0; j<m; ++j) {
                     if (i == 0 || j == 0 || i == n-1 || j == m-1) {
                         if (map[i][j] == -1) {
                             q.add(new int[]{i, j});
                             visited[i][j] = true;
-                            continue;
-                        } 
+                        }
+                        
+                        // map[i][j] = 삭제대상이라면 삭제 다음번 request에 탐색가능하게
                         if (map[i][j] == target) {
                             map[i][j] = -1;
                             visited[i][j] = true;
@@ -47,55 +54,32 @@ class Solution {
                 }
             }
             
-//             int[][] deleted = new int[n][m];
-            
-//             for (int i=0; i<n; ++i) {
-//                 // 0은 삭제 안된 노드, 1은 삭제해야하는 노드
-//                 Arrays.fill(deleted[i], 0);
-//             }
-            
-            // 테두리 타깃 미리 표시
-            // for (int i=0; i<n; ++i) {
-            //     if (map[i][0] == target) deleted[i][0] = 1;
-            //     if (map[i][m-1] == target) deleted[i][m-1] = 1;
-            // }
-            // for (int j=0; j<m; ++j) {
-            //     if (map[0][j] == target) deleted[0][j] = 1;
-            //     if (map[n-1][j] == target) deleted[n-1][j] = 1;
-            // }
-            
-            // 3-2. bfs
+            // bfs
             while (!q.isEmpty()) {
                 int[] now = q.poll();
                 
-                // 3-3. 지게차가 갈 수 있는 경로로 탐색하면서
                 for (int i=0; i<4; ++i) {
-                    int nx = now[0] + dx[i];
-                    int ny = now[1] + dy[i];
-                    if (check(n, m, nx, ny)) continue;
+                    int nx = dx[i] + now[0];
+                    int ny = dy[i] + now[1];
+                    
+                    if (check(nx, ny, n, m)) continue;
                     if (visited[nx][ny]) continue;
                     
                     if (map[nx][ny] == -1) {
                         q.add(new int[]{nx, ny});
                         visited[nx][ny] = true;
                         continue;
-                    } 
+                    }
+                    
                     if (map[nx][ny] == target) {
                         map[nx][ny] = -1;
-                        visited[nx][ny] = true;
+                        visited[nx][ny]  = true;
+                        
                     }
                 }
             }
-            
-            // 삭제 가능한 노드 삭제
-            // only 지게차
-            // for (int i=0; i<n; ++i) {
-            //     for (int j=0; j<m; ++j) {
-            //         if (deleted[i][j] == 1) map[i][j] = -1;
-            //     }
-            // }
         }
-        // map에서 삭제 된 노드 제외하고 남아있는 노드 계산
+        
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
                 if (map[i][j] != -1) answer++;
@@ -105,7 +89,7 @@ class Solution {
         return answer;
     }
     
-    boolean check(int n, int m, int x, int y) {
+    boolean check(int x, int y, int n, int m) {
         return x < 0 || y < 0 || x > n-1 || y > m-1;
     }
 }

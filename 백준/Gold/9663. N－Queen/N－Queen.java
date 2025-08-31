3e314d063d53b467ac19b95bf0f3f25403532bcc
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-08-31 10:29:40 +0900

    [Gold IV] Title: N-Queen, Time: 5980 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,46 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    static int[] answer;
+    
     static int N;
-    static int result=0;
+    static int[] map;
+    static int answer;
+    
     public static void main(String[] args) throws Exception {
-        // N만큼의 Queen을 배치해야한다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        init(Integer.parseInt(br.readLine()));
         
-        backTracking(0); // 조합
-        System.out.println(result);
+        N = Integer.parseInt(br.readLine());
+        map = new int[N];
+        
+        backTracking(0);
+        System.out.println(answer);
     }
     
-    static void init(int n) {
-        N = n;
-        answer = new int[n];
-    }
-    
-    static void backTracking(int col) {
-        if (col == N) { // 모든 열을 다돌았다. 즉 모든 queen이 가질 수 있는 조합을 완성시켰다.
-            result++;
+    static void backTracking(final int row) {
+        // 탈출 조건
+        if (row == N) {
+            answer++;
             return;
-        }
+        }     
         
         for (int i=0; i<N; ++i) {
-            answer[col] = i;
-            
-            if (check(col)) {
-                backTracking(col+1);
+            map[row] = i;
+            if (check(row)) {
+                backTracking(row+1);
             }
         }
     }
     
-    // Queen이 현재 같은 행에 있거나, 대각선에 있는지 확인
-    static boolean check(int col) {
-        for (int i=0; i<col; ++i) {
-            if (answer[i] == answer[col]) return false;
-            if (Math.abs(i - col) == Math.abs(answer[i] - answer[col])) return false;
-        }
-        
+    static boolean check(int row) {
+        for (int i=0; i<row; ++i) {
+            if (map[row] == map[i]) return false; // 같은 열에 배치시
+            if ((Math.abs(map[row] - map[i])) == Math.abs(row - i)) return false;
+        } 
         return true;
     }
 }

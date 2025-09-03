9a482235398b519cd72da7489fcd6bcb56dfac77
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-09-03 21:54:01 +0900

    [level 3] Title: N으로 표현, Time: 27.82 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,40 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if (N == number) {
-            return 1;
-        }
-        int answer = 0;
+        if (number == N) return 1;
         List<Set<Integer>> list = new ArrayList<>();
         
-        // set으로 채워주기
         for (int i=0; i<8; ++i) {
-            list.add(new HashSet<Integer>());
+            list.add(new HashSet<>());
         }
         
-        for (int i=0; i<list.size(); ++i) {
+        for (int i=0; i<8; ++i) {
             StringBuilder sb = new StringBuilder();
+            
             for (int j=0; j<=i; ++j) {
-                sb.append(N);
+                sb.append(String.valueOf(N));
             }
+            
             list.get(i).add(Integer.parseInt(sb.toString()));
         }
         
         for (int i=1; i<8; ++i) {
             for (int j=0; j<i; ++j) {
-                Set<Integer> temp = new HashSet<>();
-                for (int op1 : list.get(j)) {
-                    for (int op2 : list.get(i-j-1)) {
-                        temp.add(op1 + op2);
-                        temp.add(op1 - op2);
-                        temp.add(op1 * op2);
-                        if (op2 != 0) {
-                            temp.add(op1 / op2);
-                        }
+                //Set<Integer> temp = new HashSet<>();
+                Set<Integer> arg1 = list.get(j);
+                Set<Integer> arg2 = list.get(i-j-1);
+                for (int a : arg1) {
+                    for (int b : arg2) {
+                        list.get(i).add(a + b);
+                        list.get(i).add(a - b);
+                        list.get(i).add(a * b);
+                        if (b != 0) list.get(i).add(a / b);
                     }
                 }
-                
-                list.get(i).addAll(temp);
             }
             
             if (list.get(i).contains(number)) {

a5b7f3176a8f07d76be66c6bde63d79914fe8b68
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2025-09-28 13:31:46 +0900

    [level 2] Title: 완전범죄, Time: 0.54 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,32 +1,34 @@
 import java.util.*;
 
 class Solution {
-    static final int INF = 100000;
+    int INF = 10000;
     public int solution(int[][] info, int n, int m) {
-        int size = info.length;
-        int [][] dp = new int [size+1][m];
-        for(int i = 0; i <= size; i++){
+        int[][] dp = new int[info.length+1][m];
+        // dp[i][j] -> i는 몇번쨰 훔쳤을때 j는 b도둑이 훔친 갯수 -> dp[i][j] = 최솟값
+        
+        for (int i=0; i<info.length+1; ++i) {
             Arrays.fill(dp[i], INF);
         }
-        dp[0][0] = 0; //// 물건 0개, B의 흔적 0 → A의 흔적도 0
-        for(int i = 1; i <= size; i++){
+        dp[0][0] = 0;
+        
+        for (int i=1; i<info.length+1; ++i) {
             int a = info[i-1][0];
             int b = info[i-1][1];
-            for(int j = 0; j < m; j++){
-                // a 선택
+            
+            for (int j=0; j<m; ++j) {
                 dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + a);
-                // b 선택
-                if(j + b < m){
-                    dp[i][j + b] = Math.min(dp[i][j + b], dp[i-1][j]);
+                
+                if (j + b < m) {
+                    dp[i][j+b] = Math.min(dp[i][j+b], dp[i-1][j]);
                 }
             }
         }
-
-        //갱신 끝 정답찾기.
-        int min = INF;
-        for(int j = 0; j < m; j++){
-            min = Math.min(dp[size][j], min);
+        
+        int answer=10000;
+        for (int i=0; i<m; ++i) {
+            answer = Math.min(answer, dp[info.length][i]);
         }
-        return min >= n ? -1 : min;
+        
+        return answer >= n ? -1 : answer;
     }
 }

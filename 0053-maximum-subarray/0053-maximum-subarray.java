e45fa96a762649ae4e769ba23a0cfe465472c190
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-02-22 19:59:50 +0900

    Time: 1 ms (99.69%), Space: 79.2 MB (12.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,24 @@
 class Solution {
       public int maxSubArray(int[] nums) {
-        int maxValue = nums[0];
-        int currentSum = 0;
-        for (int num : nums) {
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
-
-            currentSum += num;
-            maxValue = Math.max(currentSum, maxValue);
+        int n = nums.length;
+        
+        // Step 1: Create DP array
+        int[] dp = new int[n];
+        
+        // Step 2: Base case
+        dp[0] = nums[0];
+        
+        // Step 3: Fill DP table
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
         }
-
-        return maxValue;
-    }
+        
+        // Step 4: Find global maximum
+        int maxSum = dp[0];
+        for (int i = 1; i < n; i++) {
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        
+        return maxSum;
+      }
 }

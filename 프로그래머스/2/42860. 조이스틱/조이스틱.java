e42dad6180176428c243effeb25b1e3430d2b9cd
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-02-23 20:55:24 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,30 +1,37 @@
 class Solution {
     public int solution(String name) {
-        int answer = 0;
-        int move = name.length() - 1;
-
-        for (int i = 0; i < name.length(); i++) {
-
-            int spellToInt = name.charAt(i) - 'A';
-            int spellToIntRev = 'Z' - name.charAt(i) + 1;
-
-            final int minResult = Math.min(spellToIntRev, spellToInt);
-            answer += minResult;
-
-            int next = i + 1;
-            while (next < name.length() && name.charAt(next) == 'A') {
-                next++;
+        // A-Z
+        // A에서 아랫쪽으로 이동하면 Z
+        // 맨 처음은 name의 갯수에 따라서 ex) 3글자면 AAA
+        // J 까지는 위로 9번 조작하여 J를 완성 가능
+        char[] c = name.toCharArray();
+        // ABCDEFGHIJKLMNOPQRSTUVWXYZ 총 26개
+        // B와 Z 사이를 나누면 J까지가 가운데
+        //System.out.println('J' - 'A'); 
+        
+        int currentCursor = 0;
+        int move = c.length-1;
+        for (int i=0; i<c.length; ++i) {
+            // 현재 커서
+            currentCursor += min(c[i]);
+            
+            int nextCursor = i+1;
+            while (nextCursor < c.length && c[nextCursor] == 'A') {
+                nextCursor++;
             }
-
-            // 순서대로 가기 vs 뒤로 돌아가기
-            move = Math.min(move, (i * 2) + name.length() - next);
-            move = Math.min(move, (name.length() - next) * 2 + i);
+            
+            move = Math.min(move, (c.length - nextCursor) + (i * 2));
+            move = Math.min(move, (c.length - nextCursor) * 2 + i);
         }
-
-        answer += move;
-
-
-
-        return answer;
+        
+        return currentCursor + move;
+    }
+    
+    int min(char c) {
+        if (c - 'A' < 'Z' - c+1) {
+            return c - 'A';
+        } else {
+            return 'Z' - c + 1;
+        }
     }
 }

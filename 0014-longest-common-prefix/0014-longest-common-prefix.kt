7e966547c70719111065fe3a773f5157ff11a65c
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-03-08 11:38:59 +0900

    Time: 1 ms (99.59%), Space: 42 MB (63.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.kt
+++ 0014-longest-common-prefix/0014-longest-common-prefix.kt
@@ -2,12 +2,12 @@ class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
         var answer = ""
         for (i in strs[0].indices) {
-            for (j in 1..strs.lastIndex) {
-                if (i == strs[j].length || strs[j][i] != strs[0][i]) {
+            for (j in 1..strs.size-1) {
+                if (i == strs[j].length || strs[0][i] != strs[j][i]) {
                     return answer
                 }
             }
-            answer += strs[0][i]
+            answer += strs[0][i];
         }
         return answer
     }

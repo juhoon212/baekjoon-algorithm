332b9e962f702bb7220838feecfcbe8d59e8a180
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-03-13 20:41:07 +0900

    Time: 1 ms (33.03%), Space: 40.3 MB (32.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.kt
+++ 0062-unique-paths/0062-unique-paths.kt
@@ -1,14 +1,15 @@
 class Solution {
     fun uniquePaths(m: Int, n: Int): Int {
         var row: Array<Int> = Array(n) { 1 }
-        
-        for (i: Int in 1 until m) {
+
+        for (i in 0 until m-1) {
             var newRow: Array<Int> = Array(n) { 1 }
-            for (j: Int in n-2 downTo 0) {
+            for (j in n - 2 downTo 0) {
                 newRow[j] = newRow[j+1] + row[j]
             }
             row = newRow
         }
+
         return row[0]
     }
 }

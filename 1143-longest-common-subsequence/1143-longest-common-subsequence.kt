82adf375bf9d48cc529548d396719d38f050335c
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-03-16 21:09:49 +0900

    Time: 29 ms (37.2%), Space: 74.4 MB (7.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.kt
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun longestCommonSubsequence(text1: String, text2: String): Int {
-        // 2차원 배열 활용
-        // dp 배열 선언
-        val t1 = text1.length
-        val t2 = text2.length
-        val dp: Array<Array<Int>> = Array(t1+1) { Array(t2+1) { 0 } }
+        var t1 = text1.length
+        var t2 = text2.length
+        var dp: Array<Array<Int>> = Array(t1+1) { Array(t2+1) { 0 } }
 
+        dp[t1-1][t2-1] = 1
         for (i in t1-1 downTo 0) {
             for (j in t2-1 downTo 0) {
                 if (text1[i] == text2[j]) {
-                    dp[i][j] = dp[i+1][j+1] + 1
+                    dp[i][j] = 1 + dp[i+1][j+1]
                 } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1])
+                    dp[i][j] = maxOf(dp[i+1][j], dp[i][j+1])
                 }
             }
         }

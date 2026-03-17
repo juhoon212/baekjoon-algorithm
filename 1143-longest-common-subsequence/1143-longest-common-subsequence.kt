346b57137461eef948fdb6f71c880cf5ec6a8591
joohoon.dev <121741140+juhoon212@users.noreply.github.com>
2026-03-17 20:32:59 +0900

    Time: 47 ms (15.81%), Space: 74.6 MB (7.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.kt
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.kt
@@ -1,14 +1,13 @@
 class Solution {
     fun longestCommonSubsequence(text1: String, text2: String): Int {
-        var t1 = text1.length
-        var t2 = text2.length
-        var dp: Array<Array<Int>> = Array(t1+1) { Array(t2+1) { 0 } }
+        val t1 = text1.length
+        val t2 = text2.length
+        val dp: Array<Array<Int>> = Array(t1+1) { Array(t2+1) { 0 } }
 
-        dp[t1-1][t2-1] = 1
-        for (i in t1-1 downTo 0) {
-            for (j in t2-1 downTo 0) {
+        for (i: Int in t1-1 downTo 0) {
+            for (j: Int in t2-1 downTo 0) {
                 if (text1[i] == text2[j]) {
-                    dp[i][j] = 1 + dp[i+1][j+1]
+                    dp[i][j] = dp[i+1][j+1] + 1
                 } else {
                     dp[i][j] = maxOf(dp[i+1][j], dp[i][j+1])
                 }

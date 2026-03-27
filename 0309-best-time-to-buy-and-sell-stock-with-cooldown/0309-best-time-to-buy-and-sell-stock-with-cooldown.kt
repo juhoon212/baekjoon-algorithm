9c4d55d147d2ee9fc4766752469158d38f615c4a
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-03-27 21:40:16 +0900

    Time: 0 ms (100%), Space: 41.4 MB (89.47%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.kt
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.kt
@@ -1,20 +1,22 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        val n: Int = prices.size;
-
-        // 전날 내가 주식을 샀는지 팔았는지 cooldown 상태인지 알아야 함 -> dp
+        var n: Int = prices.size
+        
         var buy: Int = -prices[0]
         var sell: Int = 0
-        var cooldown: Int = 0
-
-        for (i in 1 until n) {
+        var cooldown: Int = 0;
+ 
+        for (i: Int in 1 until n) {
             val prevBuy = buy;
             val prevSell = sell;
             val prevCooldown = cooldown;
 
-            buy = maxOf(prevBuy, prevCooldown - prices[i]) // 전날 샀거나, 전날 cooldown 상태에서 새로삼
+            // 오늘 사거나 전날에 팡아서 cooldown
+            buy = maxOf(prevBuy, prevCooldown - prices[i])
+            // 전날에 사서 파는것
             sell = prevBuy + prices[i]
-            cooldown = maxOf(prevSell, prevCooldown) // 전날에 팔아서 cooldown 상태, 계속 cooldown 상태인경우
+            // 전날에 사서 or 판다음에 cooldown
+            cooldown = maxOf(prevSell, prevCooldown)               
         }
 
         return maxOf(sell, cooldown)

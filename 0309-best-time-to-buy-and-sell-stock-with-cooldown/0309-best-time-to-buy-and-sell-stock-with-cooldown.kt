6f947fb8c6774aed19bf3ee78210d553f31a17bb
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-03-30 20:12:14 +0900

    Time: 1 ms (92.93%), Space: 41.5 MB (88.89%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.kt
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.kt
@@ -1,24 +1,20 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
         var n: Int = prices.size
-        
         var buy: Int = -prices[0]
         var sell: Int = 0
-        var cooldown: Int = 0;
- 
+        var cooldown: Int = 0
+
         for (i: Int in 1 until n) {
-            val prevBuy = buy;
-            val prevSell = sell;
-            val prevCooldown = cooldown;
+            var prevBuy = buy
+            var prevSell = sell
+            var prevCooldown = cooldown
 
-            // 오늘 사거나 전날에 팡아서 cooldown
-            buy = maxOf(prevBuy, prevCooldown - prices[i])
-            // 전날에 사서 파는것
+            // 사는건 전날에 cooldown에서 
+            buy = maxOf(prevCooldown - prices[i], prevBuy)
             sell = prevBuy + prices[i]
-            // 전날에 사서 or 판다음에 cooldown
-            cooldown = maxOf(prevSell, prevCooldown)               
-        }
-
+            cooldown = maxOf(prevSell, prevCooldown)
+        } 
         return maxOf(sell, cooldown)
     }
 }

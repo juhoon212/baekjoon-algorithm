fc95821bba3bcdf280196249598ad2bcf39b6c02
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-04-06 20:38:13 +0900

    Time: 18 ms (43.54%), Space: 54.2 MB (40.14%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.kt
+++ 0518-coin-change-ii/0518-coin-change-ii.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun change(amount: Int, coins: IntArray): Int {
         val n: Int = coins.size
-        val dp: Array<IntArray> = Array(n+1) { IntArray(amount+1) }
-
+        val dp: Array<IntArray> = Array(n+1) { IntArray(amount+1) { 0 } }
+        
+        // amount가 0일때는 1가지이므로 초기값 세팅
         for (i: Int in 0 .. n) {
             dp[i][0] = 1
         }
 
         for (i: Int in 1 .. n) {
             for (j: Int in 1 .. amount) {
-                if (coins[i-1] <= j) {
+                if (j >= coins[i-1]) {
                     dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j]
                 } else {
                     dp[i][j] = dp[i-1][j]
                 }
             }
         }
-
         return dp[n][amount]
     }
 }
